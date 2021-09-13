ef92126f4457ce9f612b6dfd7ffc4ab24126538b
Usuario <Usuario@DESKTOP-AC0F6J8>
2021-09-13 11:42:07 -0500

    Agregar archivos de proyecto.

---
+++ PruebaTecnica/Contact.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="PruebaTecnica.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+<div class="container">
+
+        <div class="row">
+            
+                 <div class="form-group">
+                  <label for="inputPassword4">Numero de documento</label>
+                    <asp:TextBox ID="txtnumdoc" runat="server" CssClass="form-control"></asp:TextBox>
+                     <asp:Button ID="btnconsutar" runat="server" Text="Consultar" CssClass="btn btn-primary" OnClick="btnconsutar_Click" />
+                </div>
+            <br />
+            <br />
+                <div class="form-group">
+                  <label for="inputEmail4">Tipo de Documento</label>
+                    <asp:TextBox ID="txttipodoc" runat="server" CssClass="form-control"  Enabled="False"></asp:TextBox>
+                </div>               
+            
+                <div class="form-group">
+                  <label for="inputPassword4">Nombre y apellido</label>
+                    <asp:TextBox ID="txtnombres" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Contrato Laboral</label>
+                    <asp:TextBox ID="txtcontrato" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Cargo</label>
+                    <asp:TextBox ID="txtcargo" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Riesgo Laboral(ARL)</label>
+                    <asp:TextBox ID="txtriesgo" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Fecha de inicio contrato</label>
+                    <asp:TextBox ID="txtfechainicio" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>                    
+
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Fecha final de contrato (Proyectada si se tiene)</label>
+                    <asp:TextBox ID="txtfechafinal" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Salario</label>
+                    <asp:TextBox ID="txtsalario" runat="server" CssClass="form-control" Enabled="False"></asp:TextBox>
+                </div>
+
+            <div class="alert alert-info">
+                <div class="form-group">
+                  <label for="inputPassword4">Periodo laborado</label>
+                    <asp:TextBox ID="txtperiodolaborado" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Horas Total Laboradas</label>
+                    <asp:TextBox ID="txthoralaborada" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Horas diurnas</label>
+                    <asp:TextBox ID="txtdiurnas" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Horas Nocturnas</label>
+                    <asp:TextBox ID="txtNocturnas" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Horas dominicales</label>
+                    <asp:TextBox ID="txtdominicales" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Horas festivas</label>
+                    <asp:TextBox ID="txtfestivas" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword4">Descuentos de nomina</label>
+                    <asp:TextBox ID="txtdescuentos" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="alert alert-danger" runat="server" id="idmostrarmsg" visible="false" >
+
+            </div>
+                 
+      </div>
+    <asp:Button ID="btnGuardarData" runat="server" Text="Guardar Información" CssClass="btn btn-danger" OnClick="btnGuardarData_Click"  />
+    <asp:Button ID="btnNewSearch" runat="server" Text="Nueva Consulta" CssClass="btn btn-warning" OnClick="btnNewSearch_Click"  />
+    
+</div> 
+</asp:Content>
---
+++ PruebaTecnica/Contact.aspx.cs
@@ -0,0 +1,147 @@
+﻿using Npgsql;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Web.UI;
+
+namespace PruebaTecnica
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            idmostrarmsg.Visible = false;
+        }
+
+       
+        protected void btnconsutar_Click(object sender, EventArgs e)
+        {
+            try /* Select After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["CadenaConexion"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+
+                    string sqltext = "SELECT cg.nombre as cargonombre, * FROM tipodocumento t INNER JOIN contratoslaborales c ON c.idtipodocumento = t.idtipodocumento INNER JOIN cargos cg ON cg.idcargo = c.idcargo WHERE c.numerodocumento = '"+ txtnumdoc.Text.Trim() +"' limit 1";
+
+                    cmd.CommandText = sqltext;
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        txtnumdoc.Text = dt.Rows[0]["numerodocumento"].ToString();
+                        txtcontrato.Text = dt.Rows[0]["idcontrato"].ToString();
+                        txtnombres.Text = dt.Rows[0]["primernombre"].ToString() + " - " + dt.Rows[0]["segundonombre"].ToString();
+                        txtfechainicio.Text = dt.Rows[0]["fechainicio"].ToString();
+
+                        string fechafinal = "";
+                        if (dt.Rows[0]["fechafin"] == DBNull.Value)
+                        {
+                            fechafinal = "Sin fecha";  
+                        }
+                        else{
+                            fechafinal = dt.Rows[0]["fechafin"].ToString();
+                        }
+                        txtfechafinal.Text = fechafinal;
+                        txtriesgo.Text = dt.Rows[0]["idarl"].ToString() ;
+                        txtsalario.Text = dt.Rows[0]["salario"].ToString();
+                        txttipodoc.Text = dt.Rows[0]["nombre"].ToString();
+                        txtcargo.Text = dt.Rows[0]["cargonombre"].ToString();
+                        idmostrarmsg.Visible = false;
+                    }
+                    else {
+                        Console.WriteLine("No se encontraron datos");
+                        idmostrarmsg.Visible = true;
+                        idmostrarmsg.InnerHtml = "No se encontraron datos";
+                    }
+
+                    cmd.Dispose();
+                    connection.Close();
+
+
+                }
+            }
+            catch (Exception ex) {
+                idmostrarmsg.Visible = true;
+                idmostrarmsg.InnerHtml = ex.Message.ToString();
+            }
+        }
+
+        protected void btnGuardarData_Click(object sender, EventArgs e)
+        {
+            try /* Updation After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["CadenaConexion"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+
+                    string sqltext = "INSERT INTO novedadesnomina(idcontrato, periodolaborado, horaslaboradas, horaextradiurna, horaextranocturna, horaextradominical, horaextrafestiva, descuentos, usuariocreacion, fechacreacion) ";
+                    sqltext = "VALUES(@idcontrato,@periodo,@horalaborada,@HEdiuerna,@HEnocturna,@HEdominical,@HEfestiva,@descuento,'prueba',now())";
+                    cmd = new NpgsqlCommand(sqltext, connection);
+                    
+                    cmd.Parameters.AddWithValue("@idcontrato", txtcontrato.Text);
+                    cmd.Parameters.AddWithValue("@periodo", txtperiodolaborado.Text);
+                    cmd.Parameters.AddWithValue("@horalaborada", txthoralaborada.Text);
+                    cmd.Parameters.AddWithValue("@HEdiuerna", txtdiurnas.Text);
+                    cmd.Parameters.AddWithValue("@HEnocturna", txtNocturnas.Text);
+                    cmd.Parameters.AddWithValue("@HEdominical", txtdominicales.Text);
+                    cmd.Parameters.AddWithValue("@HEfestiva", txtfestivas.Text);
+                    cmd.Parameters.AddWithValue("@descuento", txtdescuentos.Text);
+
+                    cmd.Prepare();
+
+                    cmd.ExecuteNonQuery();
+                    
+                    connection.Close();
+
+                    limpiarNovedades();
+
+                    idmostrarmsg.InnerHtml = "Se guarda la informacion correctamente";
+                    idmostrarmsg.Visible = true; 
+
+                }
+            }
+            catch (Exception ex) {
+                idmostrarmsg.InnerHtml = ex.Message.ToString();
+                idmostrarmsg.Visible = true;
+            }
+        }
+
+        private void limpiarForm() {
+            txtnumdoc.Text = "";
+            txtcontrato.Text = "";
+            txtnombres.Text = "";
+            txtfechainicio.Text = "";            
+            txtfechafinal.Text = "";
+            txtriesgo.Text = "";
+            txtsalario.Text = "";
+            txttipodoc.Text = "";
+            txtcargo.Text = "";
+            idmostrarmsg.InnerHtml = "";
+            idmostrarmsg.Visible = false;
+        }
+        private void limpiarNovedades() {
+            txtperiodolaborado.Text = "";
+            txthoralaborada.Text = "";
+            txtdiurnas.Text = "";
+            txtNocturnas.Text = "";
+            txtdominicales.Text = "";
+            txtfestivas.Text = "";
+            txtdescuentos.Text = "";
+
+        }
+
+        protected void btnNewSearch_Click(object sender, EventArgs e)
+        {
+            limpiarForm();
+        }
+    }
+}
