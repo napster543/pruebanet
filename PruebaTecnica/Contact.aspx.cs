79685967d503ac4ca78b02e2aaed3db86da5a8b6
Usuario <Usuario@DESKTOP-AC0F6J8>
2021-09-13 11:42:07 -0500

    Prueba de C# y postgret solucionadoAgregar archivos de proyecto.

---
+++ PruebaTecnica/Contact.aspx.cs
@@ -0,0 +1,147 @@
+﻿using Npgsql;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Web.UI;
+
+namespace PruebaTecnica
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            idmostrarmsg.Visible = false;
+        }
+
+       
+        protected void btnconsutar_Click(object sender, EventArgs e)
+        {
+            try /* Select After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["CadenaConexion"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+
+                    string sqltext = "SELECT cg.nombre as cargonombre, * FROM tipodocumento t INNER JOIN contratoslaborales c ON c.idtipodocumento = t.idtipodocumento INNER JOIN cargos cg ON cg.idcargo = c.idcargo WHERE c.numerodocumento = '"+ txtnumdoc.Text.Trim() +"' limit 1";
+
+                    cmd.CommandText = sqltext;
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        txtnumdoc.Text = dt.Rows[0]["numerodocumento"].ToString();
+                        txtcontrato.Text = dt.Rows[0]["idcontrato"].ToString();
+                        txtnombres.Text = dt.Rows[0]["primernombre"].ToString() + " - " + dt.Rows[0]["segundonombre"].ToString();
+                        txtfechainicio.Text = dt.Rows[0]["fechainicio"].ToString();
+
+                        string fechafinal = "";
+                        if (dt.Rows[0]["fechafin"] == DBNull.Value)
+                        {
+                            fechafinal = "Sin fecha";  
+                        }
+                        else{
+                            fechafinal = dt.Rows[0]["fechafin"].ToString();
+                        }
+                        txtfechafinal.Text = fechafinal;
+                        txtriesgo.Text = dt.Rows[0]["idarl"].ToString() ;
+                        txtsalario.Text = dt.Rows[0]["salario"].ToString();
+                        txttipodoc.Text = dt.Rows[0]["nombre"].ToString();
+                        txtcargo.Text = dt.Rows[0]["cargonombre"].ToString();
+                        idmostrarmsg.Visible = false;
+                    }
+                    else {
+                        Console.WriteLine("No se encontraron datos");
+                        idmostrarmsg.Visible = true;
+                        idmostrarmsg.InnerHtml = "No se encontraron datos";
+                    }
+
+                    cmd.Dispose();
+                    connection.Close();
+
+
+                }
+            }
+            catch (Exception ex) {
+                idmostrarmsg.Visible = true;
+                idmostrarmsg.InnerHtml = ex.Message.ToString();
+            }
+        }
+
+        protected void btnGuardarData_Click(object sender, EventArgs e)
+        {
+            try /* Updation After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["CadenaConexion"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+
+                    string sqltext = "INSERT INTO novedadesnomina(idcontrato, periodolaborado, horaslaboradas, horaextradiurna, horaextranocturna, horaextradominical, horaextrafestiva, descuentos, usuariocreacion) ";
+                    sqltext += "VALUES(@idcontrato,@periodo,@horalaborada,@HEdiuerna,@HEnocturna,@HEdominical,@HEfestiva,@descuento,@usuario)";
+
+                    cmd.CommandText = sqltext;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@idcontrato", Convert.ToInt32(txtcontrato.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@periodo", Convert.ToInt32(txtperiodolaborado.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@horalaborada", Convert.ToInt32(txthoralaborada.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@HEdiuerna", Convert.ToInt32(txtdiurnas.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@HEnocturna", Convert.ToInt32(txtNocturnas.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@HEdominical", Convert.ToInt32(txtdominicales.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@HEfestiva", Convert.ToInt32(txtfestivas.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@descuento", Convert.ToInt32(txtdescuentos.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@usuario", "desde la web"));
+
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+
+                    limpiarNovedades();
+
+                    idmostrarmsg.InnerHtml = "Se guarda la informacion correctamente";
+                    idmostrarmsg.Visible = true; 
+
+                }
+            }
+            catch (Exception ex) {
+                idmostrarmsg.InnerHtml = ex.Message.ToString();
+                idmostrarmsg.Visible = true;
+            }
+        }
+
+        private void limpiarForm() {
+            txtnumdoc.Text = "";
+            txtcontrato.Text = "";
+            txtnombres.Text = "";
+            txtfechainicio.Text = "";            
+            txtfechafinal.Text = "";
+            txtriesgo.Text = "";
+            txtsalario.Text = "";
+            txttipodoc.Text = "";
+            txtcargo.Text = "";
+            idmostrarmsg.InnerHtml = "";
+            idmostrarmsg.Visible = false;
+        }
+        private void limpiarNovedades() {
+            txtperiodolaborado.Text = "";
+            txthoralaborada.Text = "";
+            txtdiurnas.Text = "";
+            txtNocturnas.Text = "";
+            txtdominicales.Text = "";
+            txtfestivas.Text = "";
+            txtdescuentos.Text = "";
+
+        }
+
+        protected void btnNewSearch_Click(object sender, EventArgs e)
+        {
+            limpiarForm();
+        }
+    }
+}
---
+++ PruebaTecnica/Contact.aspx.designer.cs
@@ -0,0 +1,197 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PruebaTecnica
+{
+
+
+    public partial class Contact
+    {
+
+        /// <summary>
+        /// Control txtnumdoc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnumdoc;
+
+        /// <summary>
+        /// Control btnconsutar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnconsutar;
+
+        /// <summary>
+        /// Control txttipodoc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txttipodoc;
+
+        /// <summary>
+        /// Control txtnombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombres;
+
+        /// <summary>
+        /// Control txtcontrato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtcontrato;
+
+        /// <summary>
+        /// Control txtcargo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtcargo;
+
+        /// <summary>
+        /// Control txtriesgo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtriesgo;
+
+        /// <summary>
+        /// Control txtfechainicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfechainicio;
+
+        /// <summary>
+        /// Control txtfechafinal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfechafinal;
+
+        /// <summary>
+        /// Control txtsalario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtsalario;
+
+        /// <summary>
+        /// Control txtperiodolaborado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtperiodolaborado;
+
+        /// <summary>
+        /// Control txthoralaborada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txthoralaborada;
+
+        /// <summary>
+        /// Control txtdiurnas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtdiurnas;
+
+        /// <summary>
+        /// Control txtNocturnas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNocturnas;
+
+        /// <summary>
+        /// Control txtdominicales.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtdominicales;
+
+        /// <summary>
+        /// Control txtfestivas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfestivas;
+
+        /// <summary>
+        /// Control txtdescuentos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtdescuentos;
+
+        /// <summary>
+        /// Control idmostrarmsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl idmostrarmsg;
+
+        /// <summary>
+        /// Control btnGuardarData.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardarData;
+
+        /// <summary>
+        /// Control btnNewSearch.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNewSearch;
+    }
+}
